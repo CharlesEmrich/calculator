7e5914b41e6ccb03715f871b9272834f8d6647f9
Charles Emrich and Maria del Castillo <charles+maria@>
2017-04-24 11:00:23 -0700

    add initial file skeleton

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var subtract = function(num1,num2) {
+return num1 - num2;
+};
+
+var multiply = function(num1,num2) {
+return num1 * num2;
+};
+
+var threeTimes = function(num1,num2,num3) {
+return num1 * num2 * num3;
+};
+
+var divide = function(num1, num2) {
+return num1 / num2;
+};
+
+var remainder = function(num1, num2) {
+return num1 % num2;
+};
+
+var add = function(number1, number2) {
+ return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1,number2));
