28f93f7bcf4ab821279cda665e1597107a4615a0
Olena Kuchko and Phat Nguyen <olena+phat@>
2017-04-26 10:41:16 -0700

    add user interface for addition

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
+// Business (or back-end) logic:
 var subtract = function(num1,num2) {
 return num1 - num2;
 };
-
 var multiply = function(num1,num2) {
 return num1 * num2;
 };
-
 var threeTimes = function(num1,num2,num3) {
 return num1 * num2 * num3;
 };
-
 var divide = function(num1, num2) {
 return num1 / num2;
 };
-
 var remainder = function(num1, num2) {
 return num1 % num2;
 };
-
 var add = function(number1, number2) {
  return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// Front end logic:
+$(function() {
+  $("#add").submit(function(event) {
+    var firstNumber  = parseInt($("#add1").val());
+    var secondNumber = parseInt($("#add2").val());
 
-alert(add(number1,number2));
+    var result = add(firstNumber,secondNumber);
+
+    $("#output").text(result);
+    event.preventDefault();
+  })
+})
