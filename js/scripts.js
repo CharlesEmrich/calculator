70e91d8caaeba758cf1897c75403c26211669a5f
Olena Kuchko and Phat Nguyen <olena+phat@>
2017-04-26 12:55:39 -0700

    add switch functionality to extend UI to subtract, multiply, and divide operations

--- js/scripts.js
+++ js/scripts.js
@@ -18,15 +18,32 @@ var add = function(number1, number2) {
  return number1 + number2;
 };
 
+function oper (operation) {
+  console.log("Running oper() on " + operation);
+  // operation(parseInt($("#add1").val()), parseInt($("#add2").val()))
+  switch(operation) {
+  case "add":
+      return add(parseInt($("#add1").val()), parseInt($("#add2").val()));
+  case "multiply":
+      return multiply(parseInt($("#add1").val()), parseInt($("#add2").val()));
+  case "remainder":
+      return remainder(parseInt($("#add1").val()), parseInt($("#add2").val()));
+  case "divide":
+      return divide(parseInt($("#add1").val()), parseInt($("#add2").val()));
+  case "subtract":
+      return subtract(parseInt($("#add1").val()), parseInt($("#add2").val()));
+  default:
+      console.log("Defaulted. operation = " + operation);
+  }
+}
+
 // Front end logic:
 $(function() {
-  $("#add").submit(function(event) {
-    var firstNumber  = parseInt($("#add1").val());
-    var secondNumber = parseInt($("#add2").val());
-
-    var result = add(firstNumber,secondNumber);
-
-    $("#output").text(result);
+  $("#calc-form").submit(function(event) {
     event.preventDefault();
-  })
-})
+  });
+
+  $("button").click(function() {
+    $("#output").text(oper($(this)[0].id));
+    });
+  });
